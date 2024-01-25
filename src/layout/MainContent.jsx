ee1edd1c121423d5dcabe6ce0be6578f94cd06f7
Leah Nyambura <leahnyambura682@gmail.com>
2024-01-25 13:23:39 +0300

    Components, desinging using scss ,Props

---
+++ src/layout/MainContent.jsx
@@ -0,0 +1,13 @@
+import './MainContent.scss';
+
+
+const MainContent=()=>{
+    return (
+       <div className="main">
+         <h2>Main</h2>
+       </div>
+    )
+};
+
+export default MainContent;
+
